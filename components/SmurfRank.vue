e477de16a40db7ce8c3693159d337eb84c725c68
Lucía <124211865+lucialv@users.noreply.github.com>
2023-11-04 22:38:06 +0100

    Update SmurfRank.vue

--- components/SmurfRank.vue
+++ components/SmurfRank.vue
@@ -10,7 +10,7 @@
 							alt="logolucia"
 						/> -->
 						<a
-							href="https://tracker.gg/valorant/profile/riot/LuciaZapatito%23love/overview"
+							href="https://tracker.gg/valorant/profile/riot/lucia%23trans/overview"
 							target="_blank"
 							class="mx-auto font-bold hover:underline"
 							:style="{ color: getTierColor(currentTierPatched) }"
@@ -63,7 +63,7 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 
-const apiUrl = 'https://api.henrikdev.xyz/valorant/v1/mmr/eu/LuciaZapatito/love';
+const apiUrl = 'https://api.henrikdev.xyz/valorant/v1/mmr/eu/lucia/trans';
 const playerName = ref('');
 const currentTier = ref('');
 const mmr = ref(0);
